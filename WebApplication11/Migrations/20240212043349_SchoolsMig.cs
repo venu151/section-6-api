0c6ef2c0b7b74c294920f90466185637652c8886
vadde <vadde@LAPTOP-SBAQO473>
2024-02-12 13:58:45 +0530

    Add project files.

---
+++ WebApplication11/Migrations/20240212043349_SchoolsMig.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication11.Migrations
+{
+    public partial class SchoolsMig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Sch",
+                columns: table => new
+                {
+                    RollNo = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SchoolId = table.Column<int>(type: "int", nullable: false),
+                    marks = table.Column<int>(type: "int", nullable: false),
+                    Student = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Subject = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sch", x => x.RollNo);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Sch");
+        }
+    }
+}
---
+++ WebApplication11/Models/Sch.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication11.Models
+{
+ 
+        [Table("Sch")]
+        public class School
+        {
+            [Key]
+            public int RollNo { get; set; }
+            public int SchoolId { get; set; }
+            public int marks { get; set; }
+            public string Student { get; set; }
+            public string Subject { get; set; }
+
+        }
+    }
+
